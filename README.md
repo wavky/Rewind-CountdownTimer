95c68260d4afa330169ec4e5f1490294712a9530
Wavky Huang <wavky@users.noreply.github.com>
2024-06-23 20:30:20 +0900

    update Readme & build.gradle

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-Countdown Timer
+Rewind
 ==
+Initial project name: `Countdown Timer` (abbreviated as cdtimer)
+
+![clock.jpg](doc%2Fclock.jpg)
+[_Illustration by
+starglade@pixabay_](https://pixabay.com/illustrations/alarm-clock-watercolor-painting-2400092/)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
     applicationId = "com.wavky.cdtimer"
     minSdk = 30 // Android 11
     targetSdk = 34
-    versionCode = 1
+    versionCode = 3
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -145,11 +145,9 @@ dependencies {
   // androidx
   implementation("androidx.core:core-ktx:1.13.1")
   implementation("androidx.appcompat:appcompat:1.7.0")
-  implementation("androidx.cardview:cardview:1.0.0")
   implementation("androidx.fragment:fragment-ktx:1.8.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
   implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-  implementation("androidx.recyclerview:recyclerview:1.3.2")
   val navVersion = "2.7.7"
   // Kotlin
   implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
@@ -160,8 +158,6 @@ dependencies {
 
   // ------ google系 ------
   implementation("com.google.android.material:material:1.12.0")
-  implementation("com.google.code.gson:gson:2.9.1")
-  implementation("com.google.zxing:core:3.4.0")
 
   // firebase
   //  https://firebase.google.com/docs/android/learn-more#bom
@@ -175,21 +171,9 @@ dependencies {
   // glide
   implementation("com.github.bumptech.glide:glide:4.12.0")
   implementation("jp.wasabeef:glide-transformations:4.3.0")
-  // okhttp3
-  implementation("com.squareup.okhttp3:okhttp:3.14.9")
-  implementation("com.squareup.okhttp3:logging-interceptor:3.10.0")
-  // retrofit2
-  implementation("com.squareup.retrofit2:retrofit:2.9.0")
-  implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-  implementation("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
   // koin
   implementation("io.insert-koin:koin-android:3.2.2")
   implementation("io.insert-koin:koin-android-compat:3.2.2")
-  // Netty
-  implementation("io.netty:netty-all:4.1.68.Final")
-  // Jackson
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
   //  ------ debug ------
   debugImplementation("com.squareup.leakcanary:leakcanary-android:2.14")
 
